7b776f069306746e1ca96dd4bca3afdb6f9208ff
vohyz <531322336@qq.com>
2020-02-08 19:48:24 +0800

    aaa

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, make_response, url_for, redirect, render_template, session, Session
+import datetime
+import pymysql
 
 app = Flask(__name__, static_url_path = "")
 app.config['SECRET_KEY'] = '123456'
@@ -14,6 +16,34 @@ def main():
 @app.route("/login")
 def login():
     return render_template("login.html")
+@app.route('/login/in', methods=['GET', 'POST'])    
+def login_in():                                         # 用户输入信息登录
+    json_data = request.json                            # 获取数据
+    logn = json_data.get("logn")
+    pswd = json_data.get("pswd")
+    try:
+        conn, cursor = conn2mysql() 
+        sql = 'SELECT sno, sname, pswd FROM student WHERE logn = "%s"'%logn
+        cursor.execute(sql)
+
+        data = cursor.fetchone()
+        conn.commit()
+        cursor.close()
+        conn.close()
+        if data:
+            if pswd == data[2]:
+                session['sno'] = data[0]
+                session['sname'] = data[1]
+                session.permanent = True
+                app.permanent_session_lifetime = datetime.timedelta(minutes=10)
+                return jsonify(errno='ok', errmsg="登录成功")
+            else:
+                return jsonify(errno='notok', errmsg="用户名或密码错误")
+        else:
+            return jsonify(errno='notok', errmsg="用户名或密码错误")
+    except Exception as e:
+        print(e)
+        return jsonify(errno='notok', errmsg="用户数据读取失败")
 
 @app.route("/option")
 def option():
@@ -27,7 +57,11 @@ def mycourse():
 def query():
     return render_template("query.html")
 
-
+def conn2mysql():
+    conn = pymysql.connect(host="cdb-518aglpe.bj.tencentcdb.com", port=10101, user="root", password="zyx1999zyx",
+                           database="xkxt")
+    cursor = conn.cursor()
+    return conn, cursor
 
 
 
--- sqltest.py
+++ sqltest.py
@@ -3,7 +3,7 @@ import pymysql
 conn = pymysql.connect(host="cdb-518aglpe.bj.tencentcdb.com", port=10101, user="root", password="zyx1999zyx",
                        database="xkxt")
 cursor = conn.cursor()
-sql = 'ALTER TABLE `sc` ADD PRIMARY KEY USING BTREE (`cno`,`sno`)'
+sql = 'INSERT INTO student VALUES (100000, 小明, )'
 cursor.execute(sql)
 conn.commit()
 cursor.close()
