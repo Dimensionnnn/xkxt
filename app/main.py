dac02ed2d1cf54db5240e3047f05d5548dcf596d
Dimension <805180533@qq.com>
2020-02-09 19:12:50 +0800

    Initial commit

--- app/main.py
+++ app/main.py
@@ -76,18 +76,19 @@ def inputGrade():
     return render_template("inputGrade.html")
 
 
-@app.route("/grade", methods=["POST"])
+@app.route("/grade", methods=["POST", "GET"])
 def grade():
+
     json_data = request.form  # 获取数据
     cno = json_data.get("cno")  # 按课程号查询所有选课学生成绩
     print(cno)
     try:
+
         with DB() as db:
             if cno:
-                sql = 'SELECT DISTINCT student.sname,sc.sno, sc.grade FROM sc,student,course WHERE student.sno=sc.sno and course.cno=sc.cno = "%s"' % cno
+                sql = 'SELECT student.sname,sc.sno,sc.grade FROM sc,student WHERE sc.sno = student.sno and  sc.cno= "%s"' % cno
                 db.execute(sql)
                 data = db.fetchall()
-                print(data)
                 return render_template("grade.html", data=data)
             else:
                 return render_template("grade.html")
